2b2b173449e4b960e722641c6a50913e406a6677
TRAMEN <mr1.amen12@gmail.com>
2024-11-26 01:02:08 +0100

    initial commit

---
+++ lib/common/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  
+  
+  static const dommain = 'https://skilido.com';
+  static const baseUrl = '$dommain/api/development/';
+  static const apiKey = 'LfF6qEH7yeTzgCsupcx5BP';
+  static const scheme = 'academy-app';
+  
+  static final RouteObserver<ModalRoute<void>> singleCourseRouteObserver = RouteObserver<ModalRoute<void>>();
+  static final RouteObserver<ModalRoute<void>> contentRouteObserver = RouteObserver<ModalRoute<void>>();
+
+}
